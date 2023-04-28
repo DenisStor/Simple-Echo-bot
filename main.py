7a17deccaf8242c20aee2c244bb62592cda18c83
DenShizik <denisdikarvit@gmail.com>
2023-04-28 22:23:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("hello world")
-print("hello")
+import telebot
+
+bot = telebot.TeleBot("6030062228:AAGX_L_sqJWdG_-zbeRcp31swUwStq3f-MA")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
